dda5ebd8ce6241fd53cb5482f575944fb876f7a5
uyetiskin <ufukyetiskin@hotmail.com>
2022-06-23 23:36:14 +0300

    modules okey

--- 3dayofTypeScript.ts
+++ 3dayofTypeScript.ts
@@ -1,5 +1,37 @@
 //-------- Modules ---------
 
-class Person {
-    s
+/* 
+ES Modules (Ecmascript modülleri) JavaScript için bir import 
+mekanizması sağlar. Bu mekanizma sayesinde farklı dosyalar 
+içerisinde yer alan kodlar, diğer dosyalarda import ederek tekrar 
+kullanılabilir hale gelmektedir.
+
+ES modüllerinin ana hedefi tarayıcılar olsa da 2020’de NodeJS 
+desteği ile sunucularda da kullanılabilir hale gelmiştir.
+
+Her dosya bir tane export default çıkartabilir.
+
+çıkartılan fonksiyonu şu şekilde import edebiliriz: 
+
+    import Person from './Person'
+
+şeklinde alabiliriz.
+
+burada türetilmiş object type'ını export ile gönderdikten sonra şu şekilde alabiliriz.
+
+import Person, {Student} from './Person'
+
+*/
+
+
+
+export default class Person {
+    //Static ile bu veriyi almak isteyen dosyada kullanılabilir hale geliyor.
+    static isim = 'Gurbet'
+}
+
+export type Student = {
+    isim : string,
+    no : number,
+    campus : string
 }
---
+++ 3dayofTypeScript2.ts
@@ -0,0 +1,10 @@
+import Person, {Student} from "./3dayofTypeScript";
+
+console.log(Person.isim)
+
+console.log({
+    isim: 'Gurbet',
+    no : 12312,
+    campus : 'YBS',
+
+}as Student)
