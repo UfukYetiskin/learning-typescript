5b56f65f84829c2366d1b27c283742caa2f73c35
uyetiskin <ufukyetiskin@hotmail.com>
2022-06-26 19:14:15 +0300

    ts watch

---
+++ 6dayofTypeScript.ts
@@ -0,0 +1,17 @@
+// ----- Watch Mode | İzleme Modu ------
+
+/* ts uzantılı dosyaları dinlemeye alıp her değişikilte compile işleminin 
+gerçekleştirilmesi için alttaki gibi bir sözdizimi kullanabilirsiniz. Alt klasörlerin 
+tümü altındaki ts uzantılı dosyalar dinlenmeye alınacaktır. 
+
+$ tsc --watch
+
+İpucu: üstteki kodun daha kısa sözdizimi yer almaktadır oda tsc -w şeklindedir. Eğer 
+tekbir dosyayı dinlemek isterseniz tsc main.ts --watch şeklinde dinlemeye alabilirsiniz
+ belirli birden fazla dosyayı dinlemek için ise 2 komut üstünde yer alan örnek komutun
+  sonuna --watch komutunu ekleyerek belirlediğiniz birden fazla ts dosyasınıda 
+  dinlemeye alabilirsiniz.
+*/
+
+// Example of basic API 
+
