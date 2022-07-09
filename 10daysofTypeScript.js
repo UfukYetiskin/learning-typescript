a49ac1a83e306d35baab6b4acac4ddb40137b6db
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-09 23:46:19 +0300

    Acces Modifiers

--- 10daysofTypeScript.js
+++ 10daysofTypeScript.js
@@ -14,3 +14,7 @@ var Kisi = /** @class */ (function () {
 var kisiBilgisi = new Kisi(16219049, 'Ufuk', 'yetişkin');
 console.log(kisiBilgisi);
 console.log(kisiBilgisi.getFullName());
+// ---- Acces Modifiers (Public | Private | Protected)
+//Kisi class'ının özellikleri (id, firstName, lastName) varsayılan (default) olarak publictir. Publick olması sayesinde istediğimiz yerden o özelliği çekebiliyoruz.
+//Private, tanımlanılan değişken ya da özellik sadece tanımlandığı scope içerisinde kullanılabilir.
+//Protected ile class içerisinde tanımlanmış özellikler sadece class içerisinde ve classı extends alan classlar içerisinde kullanılabilir.
--- 10daysofTypeScript.ts
+++ 10daysofTypeScript.ts
@@ -2,7 +2,7 @@
 console.log('Classes')
 
 class Kisi{
-    id: number;
+    public id: number;
     firstName : string;
     lastName : string;
     constructor(id: number, firstName: string, lastName: string){
@@ -19,3 +19,12 @@ class Kisi{
 let kisiBilgisi = new Kisi(16219049, 'Ufuk' , 'yetişkin')
 console.log(kisiBilgisi)
 console.log(kisiBilgisi.getFullName())
+
+// ---- Acces Modifiers (Public | Private | Protected)
+//Kisi class'ının özellikleri (id, firstName, lastName) varsayılan (default) olarak publictir. Publick olması sayesinde istediğimiz yerden o özelliği çekebiliyoruz.
+
+//Private, tanımlanılan değişken ya da özellik sadece tanımlandığı scope içerisinde kullanılabilir.
+
+//Protected ile class içerisinde tanımlanmış özellikler sadece class içerisinde ve classı extends alan classlar içerisinde kullanılabilir.
+
+
