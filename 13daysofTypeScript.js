f724e123821b0adcbf9faf08b915674293f50d67
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-11 23:59:17 +0300

    Generics in Interfaces

--- 13daysofTypeScript.js
+++ 13daysofTypeScript.js
@@ -48,3 +48,32 @@ function merge(obj1, obj2) {
 }
 var objs = merge({ name: "Ufuk" }, { name: "Gümüş" });
 console.log(objs);
+var month = {
+    key: 2,
+    value: 'January'
+};
+console.log(month); //{key: 2, value: 'January'}
+var List = /** @class */ (function () {
+    function List() {
+        this.items = [];
+    }
+    List.prototype.add = function (o) {
+        this.items.push(o);
+        console.log(this.items);
+    };
+    List.prototype.remove = function (o) {
+        var _this = this;
+        (function (o) {
+            var index = _this.items.indexOf(o);
+            if (index > -1) {
+                _this.items.splice(index, 1);
+                console.log(_this.items);
+            }
+        });
+    };
+    return List;
+}());
+var list = new List();
+for (var i = 0; i < 10; i++) {
+    list.add(i);
+}
--- 13daysofTypeScript.ts
+++ 13daysofTypeScript.ts
@@ -56,3 +56,46 @@ let objs = merge(
     {name : "Ufuk"}, 
     {name : "Gümüş"})
 console.log(objs)
+
+
+//Interfacelerde Generic Kullanımı
+
+
+interface Months<U,V> {
+    key : U,
+    value : V
+}
+let month :Months<number, string> = {
+    key : 2,
+    value : 'January'
+}
+console.log(month) //{key: 2, value: 'January'}
+
+
+interface Collection <T> {
+    add(o : T) : void;
+    remove(o : T) : void;
+}
+
+class List<T> implements Collection<T>{
+    private items: T[] = [];
+    add(o :T) : void{
+        this.items.push(o)
+        console.log(this.items)
+    }
+    remove(o: T): void {
+        (o : T) : void{
+            let index = this.items.indexOf(o)
+            if(index > -1) {
+                this.items.splice(index, 1);
+                console.log(this.items)
+            }
+        }
+    }
+}
+
+let list = new List<number>();
+
+for(let i = 0; i<10; i++){
+    list.add(i)
+}
