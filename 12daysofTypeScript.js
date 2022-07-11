dcec584cade494acbd2f0f382038b149fc125779
uyetiskin <ufukyetiskin@hotmail.com>
2022-07-11 23:26:20 +0300

    Type Guard

--- 12daysofTypeScript.js
+++ 12daysofTypeScript.js
@@ -14,3 +14,42 @@ var musteri = {
     phone: '123123441'
 };
 console.log(musteri);
+// ---- Type Guard ---- 
+console.log('---- Type Guard ---- ');
+function add(a, b) {
+    //return a+b ile fonksiyon içerisinde veri döndüremiyoruz bu yüzden bir koşul oluşturmamız gerkemektedir.
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Types are different');
+}
+console.log(add('3', '5')); //35
+var Family = /** @class */ (function () {
+    function Family() {
+    }
+    Family.prototype.isParent = function () {
+        return true;
+    };
+    return Family;
+}());
+var NotFamiliy = /** @class */ (function () {
+    function NotFamiliy() {
+    }
+    NotFamiliy.prototype.isUncle = function () {
+        return false;
+    };
+    return NotFamiliy;
+}());
+function isAkrabalar(partner) {
+    var message;
+    if (partner instanceof Family) {
+        message = partner.isParent() ? "Evet bunlar aile" : "Aile değil";
+    }
+    if (partner instanceof NotFamiliy) {
+        message = partner.isUncle() ? "Evet bunlar aile" : "Aile değil";
+    }
+    //return message
+}
--- 12daysofTypeScript.ts
+++ 12daysofTypeScript.ts
@@ -40,3 +40,48 @@ let musteri : Customer= {
 }
 
 console.log(musteri)
+
+
+// ---- Type Guard ---- 
+console.log('---- Type Guard ---- ')
+
+type tip = string | number;
+
+function add(a : tip, b: tip){
+    //return a+b ile fonksiyon içerisinde veri döndüremiyoruz bu yüzden bir koşul oluşturmamız gerkemektedir.
+    if(typeof a === 'number' && typeof b === 'number'){
+        return a + b
+    }else if(typeof a === 'string' && typeof b === 'string'){
+        return a.concat(b);
+    }
+
+    throw new Error('Types are different')
+}
+
+console.log(add('3', '5')) //35
+
+
+class Family {
+    isParent() : boolean{
+        return true
+    }
+}
+
+class NotFamiliy{
+    isUncle() : boolean{
+        return false
+    }
+}
+
+type Akrabalar = Family | NotFamiliy
+
+function isAkrabalar(partner : Akrabalar){
+    let message : string;
+    if(partner instanceof Family){
+        message = partner.isParent() ? "Evet bunlar aile"  : "Aile değil"
+    }
+    if(partner instanceof NotFamiliy){
+        message = partner.isUncle() ? "Evet bunlar aile"  : "Aile değil"
+    }
+    //return message
+}
